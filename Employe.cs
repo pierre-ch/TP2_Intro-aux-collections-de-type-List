25fc660729521c5847d6447840e572d7e41f8133
Pierre Chevi <pierrechevi@icloud.com>
2022-02-01 12:04:49 +0100

    exemple 1 étape 1 début

---
+++ Employe.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Type_List
+{
+    class Employe
+    {
+        private string nom;
+
+        public Employe(string leNom)
+        {
+            this.nom = leNom;
+        }
+
+        public string getNom()
+        {
+            return this.nom;
+        }
+
+        public void setNom(string leNom)
+        {
+            this.nom = leNom;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,35 @@ namespace TP_Type_List
     {
         static void Main(string[] args)
         {
+
+            Employe e1 = new Employe("Poivre Romain");
+            Employe e2 = new Employe("Safran Pauline");
+            Employe e3 = new Employe("Canelle Coline");
+            Employe e4 = new Employe("Coriandre Joël");
+
+            List<string> lesEmployes = new List<string>();
+            lesEmployes.Add(e1.getNom());
+            lesEmployes.Add(e2.getNom());
+            lesEmployes.Add(e3.getNom());
+            lesEmployes.Add(e4.getNom());
+
+
+            Console.WriteLine("Liste initiale des employés");
+            for (int i = 0; i < lesEmployes.Count(); i++)
+            {
+                Console.WriteLine("rang " + i + " nom de l'employé : " + lesEmployes[i]);
+            }
+
+            lesEmployes.Insert(0, );
+
+            Console.WriteLine("Affichage de la liste après une insertion");
+
+
+           
+
+
+
+            Console.ReadKey();
         }
     }
 }
